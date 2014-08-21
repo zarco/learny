01afd684f3a0240256a4d08c042d3ba69aed63f4
Ricardo Castañeda <ricardojudo@gmail.com>
2014-08-21 15:45:25 -0500

    LRNY-54 Habilitando devise para autenticación y autorización

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'http://learny-prod.herokuapp.com', port: 80 }
 end
