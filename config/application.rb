a2b9d5bcea8a5cd7be3599b7dba0d1278df9cead
Ricardo Castañeda <ricardojudo@gmail.com>
2014-08-21 15:13:22 -0500

    LRNY-53 Habilitando internacionalización para que tome por defecto el español de México. Se agregaron los archivos de traducción en español e ingles. Utilizar funciones t('llave',default: 'default') o I18n.t('llave', default: 'default') segun corresponda (vistas, archivos *.rb)

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,22 @@ module LearnyApp
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Mexico City'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = 'es-MX'
+    
+    
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+
+      g.view_specs false
+      g.helper_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
+
   end
 end
