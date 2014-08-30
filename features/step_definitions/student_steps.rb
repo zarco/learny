1caaaf97d5ac9a6012d0dd39b975c3793673540a
Ricardo Castañeda <ricardojudo@gmail.com>
2014-08-30 16:52:45 -0500

    LRNY-42 Panel modal en home page para registro de estudiantes

--- features/step_definitions/student_steps.rb
+++ features/step_definitions/student_steps.rb
@@ -3,15 +3,15 @@ Given(/^I am browsing the site$/) do
 end
 
 When(/^I click on the sign up button$/) do
-  click_link I18n.t('devise.sessions.sign_up', default: 'Sign up')
+  click_button I18n.t('devise.sessions.sign_up', default: 'Sign up')
 end
 
 When(/^select the Sign up as student option$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link I18n.t('views.legends.student',default: 'Student')
 end
 
 Then(/^I can see the sign in form for students$/) do
-
+  visit student_registration_path
 end
 
 Given(/^I am at the sign up page$/) do
