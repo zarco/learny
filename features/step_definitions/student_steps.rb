622037545409078ce3c80830491a4bdcfa45b40f
Ricardo Castañeda <ricardojudo@gmail.com>
2014-08-30 15:16:50 -0500

    LRNY-42 Refactoring erb2haml. Agregando features student

---
+++ features/step_definitions/student_steps.rb
@@ -0,0 +1,31 @@
+Given(/^I am browsing the site$/) do
+  visit root_path
+end
+
+When(/^I click on the sign up button$/) do
+  click_button I18n.t('devise.sessions.sign_up', default: 'Sign up')
+end
+
+When(/^select the Sign up as student option$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see the sign in form for students$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am at the sign up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill in the blanks with my data$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^submit the form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see a confirmation message$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/student/student-sign-up.feature
@@ -0,0 +1,18 @@
+Feature: The student can sign up into the platform
+	In order to 
+	As a guest
+	I want to sign up as student
+	
+	Background: I am a guest on the site
+	
+	Scenario: Reaching the sign in form
+		Given I am browsing the site
+		When I click on the sign up button
+		And select the Sign up as student option
+		Then I can see the sign in form for students
+		
+	Scenario: Student's sign up successful
+		Given I am at the sign up page
+		When I fill in the blanks with my data
+		And submit the form
+		Then I can see a confirmation message
