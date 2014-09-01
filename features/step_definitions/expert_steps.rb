b5b5c463d795b734aea7d0fbdaaf930418e52902
Ricardo Castañeda <ricardojudo@gmail.com>
2014-09-01 01:10:13 -0500

    LRNY-44 Agregando escenarios para registro de expertos

---
+++ features/step_definitions/expert_steps.rb
@@ -0,0 +1,15 @@
+When(/^select the Sign up as expert option$/) do
+  click_link I18n.t('views.legends.expert',default: 'Expert')
+end
+
+Then(/^I can see the sign up form for experts$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am at the expert's sign up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I send my data for the registration as expert$/) do
+  pending # express the regexp above with the code you wish you had
+end
