4e4e4eda38c3d476bdcd6654f85cc64a5bcf89f5
Ricardo Castañeda <ricardojudo@gmail.com>
2014-09-07 22:31:04 -0500

    LRNY-59. Agregando escenarios para venue

--- features/step_definitions/venue_steps.rb
+++ features/step_definitions/venue_steps.rb
@@ -1,4 +1,6 @@
-When(/^select the Sign up as venue option$/) do
+
+When(/^select the Sign up as venue 
+option$/) do
   within(:css, '#mdl_signin') do
     click_link I18n.t('views.legends.venue',default: 'Venue')
   end
@@ -22,4 +24,51 @@ When(/^I send my data for the registration as venue$/) do
   fill_in Venue.human_attribute_name(:password_confirmation), with: @venue.password_confirmation
   fill_in Venue.human_attribute_name(:name), with: @venue.name
   click_button I18n.t('devise.sessions.sign_up')
+end
+
+
+
+Given(/^I am logged in as venue$/) do
+  @venue = FactoryGirl.create(:venue)
+  @venue.confirm!
+  visit new_venue_session_path
+  fill_in Expert.human_attribute_name(:email), with: @venue.email
+  fill_in Expert.human_attribute_name(:password), with: @venue.password
+  click_button I18n.t('devise.sessions.sign_in')
+end
+
+Given(/^I am at my home page as venue$/) do
+  visit venue_root_path
+end
+
+Given(/^the "(.*?)" calendar is selected$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click on the 'Create reservation' button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^afterwards, I sumbit the required information for the reservation$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see the new even on the "(.*?)" calendar$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click on the 'New Calendar' button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I sumbit the required data to define the "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see the new calendar listed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^afterwards, I sumbit the required information$/) do
+  pending # express the regexp above with the code you wish you had
 end
---
+++ features/venue/reservations.feature
@@ -0,0 +1,27 @@
+Feature: The venue can make a reservation
+	In order to enable spaces in my place for workshops delivery
+	As a venue
+	I want to make a reservation
+	
+Background: I am looged in as venue
+	Given I am logged in as venue	
+	
+Scenario: The venue makes a reservation in the default calendar
+	Given I am at my home page as venue
+	And the "default" calendar is selected
+	When I click on the 'Create reservation' button
+	And afterwards, I sumbit the required information for the reservation
+	Then I can see the new even on the "default" calendar
+
+Scenario: Define a new calendar
+	Given I am at my home page as venue
+	When I click on the 'New Calendar' button
+	And I sumbit the required data to define the "Eluveitie Room"
+	Then I can see the new calendar listed
+
+Scenario: The venue makes a reservation in a new calendar
+	Given I am at my home page as venue
+	And the "Eluveitie Room" calendar is selected
+	When I click on the 'Create reservation' button
+	And afterwards, I sumbit the required information
+	Then I can see the new even on the "Eluveitie Room" calendar
