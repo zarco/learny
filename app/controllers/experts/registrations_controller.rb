3587398e6b857afbe49a032ac9f8e9324d3d537a
Ricardo Castañeda <ricardojudo@gmail.com>
2014-12-08 01:29:50 -0600

    LRNY-67 Habilitando strong parameters para actualizacion de perfil de experto. Ajustando barra de herramientas

--- app/controllers/experts/registrations_controller.rb
+++ app/controllers/experts/registrations_controller.rb
@@ -11,4 +11,9 @@ class Experts::RegistrationsController < Devise::RegistrationsController
     params.require(:expert).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
+  def account_update_params
+    params.require(:expert).permit(:first_name, :last_name, :email, :password, :password_confirmation, 
+      :current_password, :occupation, :profile, :avatar, :website, :facebook_link, :linkedin_link, :twitter_link, :areas_of_expertise)
+  end
+
 end
