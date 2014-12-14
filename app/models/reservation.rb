3eaeca5473d6e8342c793c9af65a6c802d4682cf
Ricardo Castañeda <ricardojudo@gmail.com>
2014-12-13 22:27:10 -0600

    LRNY-67 FIX. Busqueda de reservaciones ya funciona sin establecer fecha

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,10 @@ class Reservation < ActiveRecord::Base
   validates :final_time, presence: true
   validates :max_participants, presence: true
 
+  scope :availables, lambda {where(:workshop => nil).where('starts_at >= ?', Date.today) }
+  scope :find_by_starts_at, lambda {|value| (where('starts_at >= (?)',  Date.parse(value))) unless value.nil? || value.empty? }
+ 
+
   extend SimpleCalendar
   has_calendar
   
@@ -16,6 +20,10 @@ class Reservation < ActiveRecord::Base
     
   def fmt_duration
     starts_at.strftime('%d-%b-%Y, %H:%M -> ') + final_time.strftime('%H:%M') if !starts_at.nil?
+  end
+
+  def self.find_availables(params)
+    availables.find_by_starts_at(params[:starts_at]).order(:starts_at)
   end
 
 end
