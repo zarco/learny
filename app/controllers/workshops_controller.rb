d31273657c2754b8a59a7ba5adcae12245e44db4
Ricardo Castañeda <ricardojudo@gmail.com>
2015-01-27 19:12:37 -0600

    LRNY-126. FIX rspec y cucumber. Disparando eventos de cambio de estado de workshops

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -41,9 +41,11 @@ class WorkshopsController < ApplicationController
   def create
     Workshop.transaction do
       @workshop = Workshop.create(workshop_params.except(:reservation_attributes).merge({:expert => current_expert}))
+      @workshop.proposed_by_expert if expert_signed_in?
       unless workshop_params[:reservation_attributes].nil?
         reservation_id=workshop_params[:reservation_attributes][:id] 
         @workshop.update_reservation(reservation_id) if @workshop.persisted? && reservation_id
+        @workshop.proposed_with_reservation
         #puts @workshop.errors.full_messages unless @workshop.valid?
         raise ActiveRecord::Rollback if @workshop.invalid?
       end        
