3c46c8ea4f8b8b8cc525fc569e871a05c2ba3007
Ricardo Castañeda <ricardojudo@gmail.com>
2015-02-11 19:13:38 -0600

    LRNY-126. FIX Se envia notificacion a establecimiento cuando se genera taller sin reservacion

--- app/models/venue_notifier.rb
+++ app/models/venue_notifier.rb
@@ -1,45 +1,43 @@
-class VenueNotifier 
-  
-  
-  
-  def workshop_proposed_by_expert(workshop) 
-    #puts "workshop_proposed_by_expert #{workshop}"
-    VenueMailer.workshop_proposed_by_expert(workshop).deliver
+class VenueNotifier
+  def workshop_proposed_by_expert(workshop)
+    unless workshop.venue.nil?
+      #puts "workshop_proposed_by_expert #{workshop}"
+      VenueMailer.workshop_proposed_by_expert(workshop).deliver
+    end
   end
-  
+
   def workshop_proposed_with_reservation(workshop)
     #puts "workshop_proposed_with_reservation #{workshop}"
     VenueMailer.workshop_proposed_with_reservation(workshop).deliver
   end
-  
+
   def accepted_by_venue(workshop)
-    #puts "accepted_by_venue #{workshop}"  
+    #puts "accepted_by_venue #{workshop}"
     VenueMailer.accepted_by_venue(workshop).deliver
   end
-  
+
   def workshop_has_been_given(workshop)
     #puts "workshop_has_been_given #{workshop}"
     VenueMailer.workshop_has_been_given(workshop).deliver
   end
-  
-  def workshop_cancelled_by_expert(workshop) 
+
+  def workshop_cancelled_by_expert(workshop)
     #puts "workshop_cancelled_by_expert #{workshop}"
     VenueMailer.workshop_cancelled_by_expert(workshop).deliver
   end
-  
+
   def workshop_revoked_by_venue(workshop)
     #puts "workshop_revoked_by_venue #{workshop}"
     VenueMailer.workshop_revoked_by_venue(workshop).deliver
   end
-    
+
   def workshop_posposed_by_expert(workshop)
     #puts "workshop_posposed_by_expert #{workshop}"
     VenueMailer.workshop_posposed_by_expert(workshop).deliver
   end
-  
+
   def reservation_updated(venue,former_workshop)
     VenueMailer.reservation_updated(venue,former_workshop).deliver
   end
-  
-  
+
 end
