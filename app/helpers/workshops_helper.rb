c011e76829a8646431a7a7ce9ace44cfbf85c2dd
Jonathan Delgado <delgado.jonathan10@gmail.com>
2015-02-21 17:11:54 -0600

    LRNY-124 Agregando hint dinamico al campo de busqueda de cursos

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -1,2 +1,7 @@
 module WorkshopsHelper
+	def random_workshop
+		hint_workshop = Workshop.select(:name).limit(1).order("random()")
+		hint_workshop_name = hint_workshop.nil? ? "surf" : hint_workshop[0].name
+		"Clases de " << hint_workshop_name
+	end
 end
