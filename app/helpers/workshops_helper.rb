81fbdd38ddf3579761fb308a5bc1f4d1f1f2856b
Jonathan Delgado <delgado.jonathan10@gmail.com>
2015-03-15 20:33:08 -0600

    LRNY-146 Mejorando campo de biografia del experto en el detalle del workshop

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -4,4 +4,8 @@ module WorkshopsHelper
 		hint_workshop_name = hint_workshop.nil? ? "surf" : hint_workshop.name
 		"Clases de " << hint_workshop_name
 	end
+
+	def small_description_profile(description,total_words=300)
+    	description_visible = description.truncate(total_words,separator: ' ')
+  	end
 end
