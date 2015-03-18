c2bf88f905e829d968ecbe1102ef339ad9f950cd
Jonathan Delgado <delgado.jonathan10@gmail.com>
2015-03-18 10:08:01 -0600

    LRNY-144 Mejorando hint de busqueda de cursos

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -1,7 +1,5 @@
 module WorkshopsHelper
 	def random_workshop
-		hint_workshop = Workshop.select(:name).limit(1).order("random()").first
-		hint_workshop_name = hint_workshop.nil? ? "surf" : hint_workshop.name
-		"Clases de " << hint_workshop_name
+		Workshop.select(:name).limit(1).order("random()").first.name		
 	end
 end
