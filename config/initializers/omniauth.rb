416bcc96d84d44f8c1dd3a01b5d60e9c6209aa11
Jonathan Delgado <delgado.jonathan10@gmail.com>
2015-05-07 18:19:04 -0500

    LRNY-147 Cambiando credenciales para la aplicacion de twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'FPBJE4oqzHf5r4HKPBkMyzBF8', 'KCPdJvcpgumI6kuOpYVw96zGKG4XvRoqqJcc2ETtqYDnUiZUHZ'
+  provider :twitter, 'sVkJcR5xEU7hNMliyb5YNPdHn', 'BHSIKEfFy4oBmmHEWat2XEg2clqhtN6Vjm2uB7c4Tmorusr6UF'
   provider :facebook, '932093316821488', 'ecac88818563adeba52f03c2683c82be'
 end
